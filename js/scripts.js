67dab9311245000f27a3553932c5dbfed4b244cf
T-Pirozzini <tpirozzini@gmail.com>
2021-10-28 16:00:13 -0700

    add RPS Game with UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //Scorekeeping
-let computerScore = 0;
-let playerScore = 0;
+let computerPoints = 0;
+let playerPoints = 0;
 
 
 // AI randomly chooses Rock, Paper or Scissors
@@ -8,53 +8,129 @@ let compOptions =  ['rock', 'paper', 'scissors']
 
 function computerPlay(){
     let computerSelection = Math.floor(Math.random() * compOptions.length);
-    return (compOptions[computerSelection]);
+    return (compOptions[computerSelection]);       
 }
 
-
 // Play One Round of RPS: Declare the winner && Award 1 point to the winner 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+  let computerSelection = computerPlay();
   // Draw  
     if (playerSelection === computerSelection) {
       console.log('It\'s a draw');
+      result.textContent = 'It\'s a draw';
   // Rock Selection
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-      computerScore++;
+      computerPoints++;
       console.log('Paper beats rock! You Lose!');
+      result.textContent = 'Paper beats rock! You Lose!';
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-      playerScore++;
+      playerPoints++;
       console.log('Rock beats scissors! You Win!');
+      result.textContent = 'Rock beats scissors! You Win!';
   // Paper Selection
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-      computerScore++;
+      computerPoints++;
       console.log('Scissors beats paper! You Lose!');
+      result.textContent = 'Scissors beats paper! You Win!';
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-      playerScore++;
-      console.log('paper beats rock! You Win!');
+      playerPoints++;
+      console.log('Paper beats rock! You Win!');
+      result.textContent = 'Paper beats rock! You Win!';
   // Scissors Selection  
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-      computerScore++;
+      computerPoints++;
       console.log('Rock beats scissors! You Lose!');
+      result.textContent = 'Rock beats scissors! You Lose!';
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-      playerScore++;
+      playerPoints++;
       console.log('Scissors beats paper! You Win!');
+      result.textContent = 'Scissors beats paper! You Win!';
     } else {
       console.log('Please input a valid response.');
     }
+    userScore.textContent = 'PLAYER SCORE:' + playerPoints;
+    compScore.textContent = 'COMPUTER SCORE:' + computerPoints;
+    compSelection.textContent = 'Computer Chose ' + computerSelection;
+    
+    if (computerPoints === 5){
+      computerWins.textContent = "Uh Oh! You Lost to the Computer!"
+
+    }
+    if (playerPoints === 5) {
+      playerWins.textContent = "Congratulations! You beat the Computer!"
+      window.addEventListener('click', refreshPage());
+    }
 }
 
-let computerSelection = computerPlay();
 
+//EventListeners. Call playRound() on 'click'
+const rock = document.querySelector('.option1');
+const paper = document.querySelector('.option2');
+const scissors = document.querySelector('.option3');
+
+rock.addEventListener('click', e => {
+  playRound('rock');  
+  })
+paper.addEventListener('click', e => {
+  playRound('paper');  
+  })
+scissors.addEventListener('click', e => {
+  playRound('scissors');  
+  })
+
+
+
+//Results
+const container = document.querySelector('#container');
+
+
+const userScore = document.createElement('div');
+userScore.classList.add('userScore');
+userScore.textContent = 'PLAYER SCORE:' + playerPoints;
+
+const compScore = document.createElement('div');
+compScore.classList.add('compScore');
+compScore.textContent = 'COMPUTER SCORE:' + computerPoints;
+
+const compSelection = document.createElement('div');
+compSelection.classList.add('compSelection');
+//compScore.textContent = 'COMPUTER SCORE:' + computerSelection;
+
+const result = document.createElement('div');
+result.classList.add('result');
+
+const playerWins = document.createElement('div');
+playerWins.classList.add('playerWins');
+
+const computerWins = document.createElement('div');
+computerWins.classList.add('computerWins');
+
+
+container.appendChild(userScore);
+container.appendChild(compScore);
+container.appendChild(compSelection);
+container.appendChild(result);
+container.appendChild(playerWins);
+container.appendChild(computerWins);
+
+//Refresh window/New Game
+function refreshPage(){
+  reload = location.reload();
+}
+
+
+
+/*~~ PREVIOUS VERSION CODE ~~
 //Take Player/Computer Input && Display Result
 function game() {
-  const playerSelection = prompt ('Choose: Rock, Paper or Scissors.').toLowerCase(); //Player responses Case Insensitive
+  const playerSelection = //prompt ('Choose: Rock, Paper or Scissors.').toLowerCase(); //Player responses Case Insensitive
   computerSelection = computerPlay();
   playRound(playerSelection, computerSelection);
   console.log('PLAYER:' + playerScore,' COMPUTER:' + computerScore);
 }
 
 //Play Five Rounds without Using Loops - Declare a Winner/Loser
-game();
+/*game();
 game();
 game();
 game();
@@ -66,10 +142,7 @@ if (playerScore > computerScore) {
 } else {
   console.log('IT\'S A DRAW! Try again?');
 }
-
-
-
-
+*/
 
 
 
