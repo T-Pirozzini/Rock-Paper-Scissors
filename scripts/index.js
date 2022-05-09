cc2894d76e8de2be6389a951639367b02b13deaf
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-05-09 14:20:14 -0700

    fix bugs and revise nulk code logic

--- scripts/index.js
+++ scripts/index.js
@@ -2,35 +2,82 @@
 let computerPoints = 0;
 let playerPoints = 0;
 
-// AI randomly chooses Rock, Paper or Scissors
+// AI Selection
 let compOptions =  ['rock', 'paper', 'scissors'];
 
 function computerPlay(){
-    let computerSelection = Math.floor(Math.random() * compOptions.length);    
-    return (compOptions[computerSelection]);       
-}
+  let computerSelection = Math.floor(Math.random() * compOptions.length);    
+  return (compOptions[computerSelection]);       
+};
 
-// Play One Round of RPS: Declare the winner && Award 1 point to the winner 
+// Player Selection
+const rock = document.querySelector('.option-rock');
+const paper = document.querySelector('.option-paper');
+const scissors = document.querySelector('.option-scissors');
+
+rock.addEventListener('click', e => {    
+    playRound('rock');  
+});
+paper.addEventListener('click', e => {
+  playRound('paper');  
+});
+scissors.addEventListener('click', e => {
+  playRound('scissors');  
+});
+
+// Display Results
+const container = document.querySelector('#results-container');
+
+const compSelection = document.createElement('div');
+compSelection.classList.add('compSelection');
+container.appendChild(compSelection);
+compSelection.textContent = 'Computer is waiting for your selection.';
+
+const result = document.createElement('div');
+result.classList.add('result');
+container.appendChild(result);
+result.textContent = 'Rock, Paper or Scissors?';
+
+const playerWins = document.createElement('div');
+playerWins.classList.add('winner');
+container.appendChild(playerWins);
+
+const computerWins = document.createElement('div');
+computerWins.classList.add('winner');
+container.appendChild(computerWins);
+
+const userScore = document.createElement('div');
+userScore.classList.add('userScore');
+userScore.textContent = 'PLAYER SCORE: ' + playerPoints;
+container.appendChild(userScore);
+
+const compScore = document.createElement('div');
+compScore.classList.add('compScore');
+compScore.textContent = 'COMPUTER SCORE: ' + computerPoints;
+container.appendChild(compScore);
+
+// Play One Round of RPS: Declare the winner && Award 1 point to the winner
 function playRound(playerSelection) {
-  let computerSelection = computerPlay();
-  // Draw  
+  let computerSelection = computerPlay(); 
+  if (playerPoints < 5 && computerPoints < 5){    
+    // Draw  
     if (playerSelection === computerSelection) {      
       result.textContent = 'It\'s a draw';
-  // Rock Selection
+    // Rock Selection
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
       computerPoints++;     
       result.textContent = 'Paper beats rock! You Lose!';
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
       playerPoints++;      
       result.textContent = 'Rock beats scissors! You Win!';
-  // Paper Selection
+    // Paper Selection
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
       computerPoints++;      
       result.textContent = 'Scissors beats paper! You Win!';
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
       playerPoints++;      
       result.textContent = 'Paper beats rock! You Win!';
-  // Scissors Selection  
+    // Scissors Selection  
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
       computerPoints++;
       result.textContent = 'Rock beats scissors! You Lose!';
@@ -40,105 +87,23 @@ function playRound(playerSelection) {
     } else {      
       result.textContent = 'Please input a valid response.';
     }
-    userScore.textContent = 'PLAYER SCORE:' + playerPoints;
-    compScore.textContent = 'COMPUTER SCORE:' + computerPoints;
-    compSelection.textContent = 'Computer Chose ' + computerSelection;
-    
-    if (computerPoints === 5){
-      computerWins.textContent = "Uh Oh! You Lost to the Computer!"
-      document.querySelector('Button').disabled = true;     
-    }
-    if (playerPoints === 5) {
-      playerWins.textContent = "Congratulations! You beat the Computer!"
-      document.querySelector('Button').disabled = true;      
-    }
+  }
+  // Display updated score
+  userScore.textContent = 'PLAYER SCORE: ' + playerPoints;
+  compScore.textContent = 'COMPUTER SCORE: ' + computerPoints;
+  compSelection.textContent = 'Computer Chose: ' + computerSelection.toUpperCase();
+
+  // Announce winner  
+  if (computerPoints === 5){    
+    computerWins.textContent = "Uh Oh! You Lost to the Computer!";
+    result.textContent = '';
+    compSelection.textContent = '';  
+  }
+  if (playerPoints === 5) {    
+    playerWins.textContent = "Congratulations! You beat the Computer!";
+    result.textContent = '';
+    compSelection.textContent = ''; 
+  } 
 }
 
-
-//EventListeners. Call playRound() on 'click'
-const rock = document.querySelector('.option1');
-const paper = document.querySelector('.option2');
-const scissors = document.querySelector('.option3');
-
-rock.addEventListener('click', e => {
-  playRound('rock');  
-  })
-paper.addEventListener('click', e => {
-  playRound('paper');  
-  })
-scissors.addEventListener('click', e => {
-  playRound('scissors');  
-  })
-
-
-
-//Results
-const container = document.querySelector('#container');
-
-
-const userScore = document.createElement('div');
-userScore.classList.add('userScore');
-userScore.textContent = 'PLAYER SCORE:' + playerPoints;
-
-const compScore = document.createElement('div');
-compScore.classList.add('compScore');
-compScore.textContent = 'COMPUTER SCORE:' + computerPoints;
-
-const compSelection = document.createElement('div');
-compSelection.classList.add('compSelection');
-//compScore.textContent = 'COMPUTER SCORE:' + computerSelection;
-
-const result = document.createElement('div');
-result.classList.add('result');
-
-const playerWins = document.createElement('div');
-playerWins.classList.add('playerWins');
-
-const computerWins = document.createElement('div');
-computerWins.classList.add('computerWins');
-
-
-container.appendChild(userScore);
-container.appendChild(compScore);
-container.appendChild(compSelection);
-container.appendChild(result);
-container.appendChild(playerWins);
-container.appendChild(computerWins);
-
-//Refresh window/New Game
-function refreshPage(){
-  reload = location.reload();
-}
-
-//Disable Buttons & Create "Try Again Button"
-
-
-
-
-/*~~ PREVIOUS VERSION CODE ~~
-//Take Player/Computer Input && Display Result
-function game() {
-  const playerSelection = //prompt ('Choose: Rock, Paper or Scissors.').toLowerCase(); //Player responses Case Insensitive
-  computerSelection = computerPlay();
-  playRound(playerSelection, computerSelection);
-  console.log('PLAYER:' + playerScore,' COMPUTER:' + computerScore);
-}
-
-//Play Five Rounds without Using Loops - Declare a Winner/Loser
-/*game();
-game();
-game();
-game();
-game();
-if (playerScore > computerScore) {
-  console.log('CONGRATULATIONS! You won the game!');
-} else if (playerScore < computerScore) {
-  console.log('YOU LOST THE GAME! Better luck next time!');
-} else {
-  console.log('IT\'S A DRAW! Try again?');
-}
-*/
-
-
-
-
+ 
